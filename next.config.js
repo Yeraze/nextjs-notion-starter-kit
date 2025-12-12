f3fd0cb738689a3de915661b3dff34f9d331f16f
Randall Hand <randall.hand@gmail.com>
2025-12-12 08:53:10 -0500

    Tidy up

--- next.config.js
+++ next.config.js
@@ -19,13 +19,6 @@ export default withBundleAnalyzer({
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com',
       'transitivebullsh.it'
-    remotePatterns: [
-      { protocol: 'https', hostname: 'www.notion.so' },
-      { protocol: 'https', hostname: 'notion.so' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'abs.twimg.com' },
-      { protocol: 'https', hostname: 'pbs.twimg.com' },
-      { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' }
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
