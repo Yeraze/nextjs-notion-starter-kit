907f637ecd39b7b62312720d07d816536b342290
Randall Hand <randall.hand@gmail.com>
2025-12-12 09:40:46 -0500

    Next.js upgrade for CVE remediation

--- next.config.js
+++ next.config.js
@@ -11,19 +11,22 @@ const withBundleAnalyzer = bundleAnalyzer({
 export default withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
   images: {
-    domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
+    remotePatterns: [
+      { hostname: 'www.notion.so' },
+      { hostname: 'notion.so' },
+      { hostname: 'images.unsplash.com' },
+      { hostname: 'pbs.twimg.com' },
+      { hostname: 'abs.twimg.com' },
+      { hostname: 's3.us-west-2.amazonaws.com' },
+      { hostname: 'transitivebullsh.it' }
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   },
+
+  // Next.js 16 uses Turbopack by default
+  turbopack: {},
 
   webpack: (config) => {
     // Workaround for ensuring that `react` and `react-dom` resolve correctly
